3c1c0d2b1d77717b7b93ed5ad01a2098897a9ad9
awegmann <wegmann@web.de>
2013-08-28 22:22:15 +0200

    added comments and paramter checks

--- src/main/groovy/de/wegmann/wrapper/ffmpeg/Ffmpeg.groovy
+++ src/main/groovy/de/wegmann/wrapper/ffmpeg/Ffmpeg.groovy
@@ -32,28 +32,51 @@ class Ffmpeg {
         this.tools = ToolsLocation.getInstance()
     }
 
+    /**
+     * Converts the sourceAudioFile to the destinationAudioFile.
+     *
+     * @param destinationAudioFile Name of destination file. Must end with ".m4a".
+     */
     public void convertToM4a(String destinationAudioFile) {
-        def process = [ tools.ffmpegExecutable, "-i", sourceAudioFile, "-strict", "-2", "-y", destinationAudioFile ].execute()
+        if (!destinationAudioFile.endsWith(".m4a")) {
+            throw new IllegalArgumentException("destination file name does not end with '.m4a' <$destinationAudioFile")
+        }
+
+        def process = [tools.ffmpegExecutable, "-i", sourceAudioFile, "-strict", "-2", "-y", destinationAudioFile].execute()
         process.err.eachLine {
             log.info "> $it"
         }
     }
 
+
+    /**
+     * Converts the sourceAudioFile to the destinationAudioFile.
+     *
+     * @param destinationAudioFile Name of destination file. Must end with ".m4b".
+     */
     public void convertToM4b(String destinationAudioFile) {
         if (!destinationAudioFile.endsWith(".m4b")) {
             throw new IllegalArgumentException("destination file name does not end with '.m4b' <$destinationAudioFile")
         }
         //def process = [ tools.ffmpegExecutable, "-i", sourceAudioFile, "-codec", "aac", "-f","mp4","-strict", "-2", "-y", destinationAudioFile-".m4b"+".m4a" ].execute()
         def intermediateFile = destinationAudioFile - ".m4b" + "-intermediate.m4a"
-        def process = [ tools.ffmpegExecutable, "-i", sourceAudioFile, "-strict", "-2", "-y", intermediateFile].execute()
+        def process = [tools.ffmpegExecutable, "-i", sourceAudioFile, "-strict", "-2", "-y", intermediateFile].execute()
         process.err.eachLine {
             log.info "> $it"
         }
         new File(intermediateFile).renameTo(new File(destinationAudioFile))
     }
 
+    /**
+     * Converts the sourceAudioFile to the destinationAudioFile.
+     *
+     * @param destinationAudioFile Name of destination file. Must end with ".flac".
+     */
     public void convertToFlac(String destinationAudioFile) {
-        def process = [ tools.ffmpegExecutable, "-i", sourceAudioFile, "-y", destinationAudioFile ].execute()
+        if (!destinationAudioFile.endsWith(".flac")) {
+            throw new IllegalArgumentException("destination file name does not end with '.flac' <$destinationAudioFile")
+        }
+        def process = [tools.ffmpegExecutable, "-i", sourceAudioFile, "-y", destinationAudioFile].execute()
         process.err.eachLine {
             log.info "> $it"
         }
