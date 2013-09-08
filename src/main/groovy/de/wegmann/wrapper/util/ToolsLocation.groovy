b2ac627b781c8a9358ce51ad3a8555a1cc8e94bb
awegmann <wegmann@web.de>
2013-09-08 21:39:06 +0200

    first changes for config file to set up tools location

--- src/main/groovy/de/wegmann/wrapper/util/ToolsLocation.groovy
+++ src/main/groovy/de/wegmann/wrapper/util/ToolsLocation.groovy
@@ -4,14 +4,23 @@ package de.wegmann.wrapper.util
  * Class to supply native tools locations.
  */
 class ToolsLocation {
-	private static final INSTANCE = new ToolsLocation()
-	
-	static ToolsLocation getInstance(){
-		return INSTANCE
-	}
+    private static final INSTANCE = new ToolsLocation()
 
-	private ToolsLocation() {
-	}
+    static ToolsLocation getInstance() {
+        return INSTANCE
+    }
+
+    private ToolsLocation() {
+        def homeConfig = new File('audibletools.config')
+        if (homeConfig.exists()) {
+            def config = new ConfigSlurper().parse(homeConfig.toURL())
+            mp4ArtExecutable = config.locations.mp4art
+            mp4InfoExecutable = config.locations.mp4info
+            mp4TagsExecutable = config.locations.mp4tags
+            mp4ChapsExecutable = config.locations.mp4chaps
+            ffmpegExecutable = config.locations.ffmpeg
+        }
+    }
 
     String mp4ArtExecutable = "mp4art"
     String mp4InfoExecutable = "mp4info"
