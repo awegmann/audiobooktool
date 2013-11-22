6746215a0272880b9d9fb0a1daa156754fcd159b
Oliver Wagner <oliver.wagner@qutico.de>
2013-11-22 09:50:48 +0100

    all executions should use this method. therefore it is possible to make a standard catching of errors, standard logging, standard time messuring, a.s.o.

--- src/main/groovy/de/audiobooktool/convert/commands/AbstractToolCommand.groovy
+++ src/main/groovy/de/audiobooktool/convert/commands/AbstractToolCommand.groovy
@@ -4,7 +4,9 @@ package de.audiobooktool.convert.commands
  * Date: 03.09.13
  */
 abstract class AbstractToolCommand implements ToolCommandIF {
-    boolean dryRun
+
+  static long totalExecutionDurationSeconds = 0
+  boolean dryRun
 
     @Override
     void setDryRun(boolean dryRun) {
@@ -21,5 +23,55 @@ abstract class AbstractToolCommand implements ToolCommandIF {
      * @param data Parameters for conversion
      */
     abstract public void init(ConversionParameterObj data);
+
+  /**
+   * Executes the command, without showing the output at default out and err. The method
+   * is waiting to return until the process ended.
+   *
+   * @param command the command to execute
+   * @return the process object
+   */
+  def static runCommand(command) {
+    return runCommand(command, false, null)
+  }
+
+  /**
+   * Executes the command and the method is waiting to return until the process ended.
+   *
+   * @param command the command to execute
+   * @param outputAtCommandLine true means show output at command line
+   * @return the process object
+   */
+  def static runCommand(command, outputAtCommandLine) {
+    return runCommand(command, outputAtCommandLine, null)
+  }
+
+  /**
+   * Executes the command ...
+   *
+   * @param command the command to execute
+   * @param outputAtCommandLine
+   * @param timeoutWaiting
+   * @return the process object
+   */
+  def static runCommand(command, outputAtCommandLine, timeoutWaiting) {
+    Date startCmd = new Date()
+    Process process = command.execute()
+    if (outputAtCommandLine)
+      process.consumeProcessOutput(System.out, System.err)
+    if (timeoutWaiting == null)
+      process.waitFor()
+    else
+      process.waitForOrKill(timeoutWaiting)
+    Date endCmd = new Date()
+    // TODO: use logger ...
+    long durationSeconds = (endCmd.getTime()-startCmd.getTime())/1000
+    totalExecutionDurationSeconds += durationSeconds
+    println "********************************************************************************************"
+    println "********** Executions duration " + durationSeconds + " seconds!! (total exec seconds: " + totalExecutionDurationSeconds + ")"
+    println "********************************************************************************************"
+    return process
+  }
+
 
 }
